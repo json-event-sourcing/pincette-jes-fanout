b1bb4582fc4e4b473789d3e5fb2ce19dfe21e782
Werner Donné <werner.donne@pincette.biz>
2020-03-23 17:49:31 +0100

    Initial version

---
+++ README.md
@@ -0,0 +1,39 @@
+# JSON Event Sourcing Fanout Service
+
+This microservice sends messages from Kafka topics to the [fanout.io](https://fanout.io) service. The topics should use the [serialisation of JSON Event Sourcing](https://www.javadoc.io/static/net.pincette/pincette-jes-util/1.1.3/net/pincette/jes/util/JsonSerde.html). The usernames from the messages are used for the channel names. They are extracted from the ```_jwt/sub``` and ```_subscriptions/sub``` fields.
+
+## Configuration
+
+The configuration is managed by the 
+[Lightbend Config package](https://github.com/lightbend/config). By default it will try to load ```conf/application.conf```. An alternative configuration may be loaded by adding ```-Dconfig.resource=myconfig.conf```, where the file is also supposed to be in the ```conf``` directory. The following entries are available.
+
+|Entry|Description|
+|---|---|
+|environment|The name of the environment, e.g. "tst", "prd".|
+|kafka|All Kafka settings come below this entry. So for example, the setting ```bootstrap.servers``` would go to the entry ```kafka.bootstrap.servers```.|
+|kafka.num.stream.threads|The number of worker threads per instance.|
+|kafka.replication.factor|When using Confluent Cloud this should be 3.|
+|logLevel|The log level as defined in [java.util.logging.Level](https://docs.oracle.com/javase/8/docs/api/java/util/logging/Level.html).|
+|logTopic|The Kafka topic where the errors will be logged in the [Elastic Common Schema](https://www.elastic.co/guide/en/ecs/current/index.html).|
+|realmIn|The realm ID for the fanout.io service.|
+|realmKey|The realm key for the fanout.io service.|
+|topics|The list of Kafka topics from which messages should be propagated.|
+
+## Building and Running
+
+You can build the tool with ```mvn clean package```. This will produce a self-contained JAR-file in the ```target``` directory with the form ```pincette-jes-fanout-<version>-jar-with-dependencies.jar```. You can launch this JAR with ```java -jar```, without further options.
+
+The total number of threads across all the instances should not exceed the number of partitions for the Kafka topics. Additional threads will be idle.
+
+You can run the JVM with the option ```-mx128m```.
+
+## Docker
+
+Docker images can be found at [https://hub.docker.com/repository/docker/jsoneventsourcing/pincette-jes-fanout](https://hub.docker.com/repository/docker/jsoneventsourcing/pincette-jes-fanout). You should add a configuration layer with a Docker file that looks like this:
+
+```
+FROM registry.hub.docker.com/jsoneventsourcing/pincette-jes-fanout:<version>
+COPY conf/tst.conf /conf/application.conf
+```
+
+So wherever your configuration file comes from, it should always end up at ```/conf/application.conf```.
---
+++ src/main/java/net/pincette/jes/fanout/Application.java
@@ -0,0 +1,74 @@
+package net.pincette.jes.fanout;
+
+import static java.lang.System.exit;
+import static java.util.logging.Level.INFO;
+import static java.util.logging.Level.parse;
+import static java.util.logging.Logger.getLogger;
+import static net.pincette.jes.elastic.Logging.log;
+import static net.pincette.jes.util.Configuration.loadDefault;
+import static net.pincette.jes.util.Fanout.connect;
+import static net.pincette.jes.util.Kafka.createReliableProducer;
+import static net.pincette.jes.util.Kafka.fromConfig;
+import static net.pincette.jes.util.Streams.start;
+import static net.pincette.util.Util.tryToDoWithRethrow;
+import static net.pincette.util.Util.tryToGetSilent;
+
+import com.typesafe.config.Config;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import net.pincette.jes.util.JsonSerializer;
+import net.pincette.jes.util.Streams;
+import org.apache.kafka.common.serialization.StringSerializer;
+import org.apache.kafka.streams.StreamsBuilder;
+import org.apache.kafka.streams.Topology;
+
+/**
+ * A microservice that forwards all messages to fanout.io channels. The usernames are used as the
+ * channel names. They are extracted from the fields <code>_jwt/sub</code> and <code>
+ * _subscriptions/sub</code>.
+ *
+ * @author Werner Donn\u00e9
+ */
+public class Application {
+  private static final String ENVIRONMENT = "environment";
+  private static final String KAFKA = "kafka";
+  private static final String LOG_LEVEL = "logLevel";
+  private static final String LOG_TOPIC = "logTopic";
+  private static final String REALM_ID = "realmId";
+  private static final String REALM_KEY = "realmKey";
+  private static final String TOPICS = "topics";
+  private static final String VERSION = "1.0";
+
+  public static void main(final String[] args) {
+    final StreamsBuilder builder = new StreamsBuilder();
+    final Config config = loadDefault();
+    final String environment = tryToGetSilent(() -> config.getString(ENVIRONMENT)).orElse("dev");
+    final Level logLevel = parse(tryToGetSilent(() -> config.getString(LOG_LEVEL)).orElse("INFO"));
+    final String logTopic =
+        tryToGetSilent(() -> config.getString(LOG_TOPIC)).orElse("log-" + environment);
+    final Logger logger = getLogger("pincette-jes-fanout");
+    final String realmId = config.getString(REALM_ID);
+    final String realmKey = config.getString(REALM_KEY);
+
+    logger.setLevel(logLevel);
+
+    config.getList(TOPICS).stream()
+        .map(value -> value.unwrapped().toString())
+        .forEach(topic -> connect(builder.stream(topic), realmId, realmKey, logger));
+
+    tryToDoWithRethrow(
+        () ->
+            createReliableProducer(
+                fromConfig(config, KAFKA), new StringSerializer(), new JsonSerializer()),
+        producer -> {
+          final Topology topology = builder.build();
+
+          log(logger, logLevel, VERSION, environment, producer, logTopic);
+          logger.log(INFO, "Topology:\n\n {0}", topology.describe());
+
+          if (!start(topology, Streams.fromConfig(config, KAFKA))) {
+            exit(1);
+          }
+        });
+  }
+}
